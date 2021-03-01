28db1cbbd15f8fbf8c8b4be22c641089bc129f9f
Bogdan Khodchenko <khoch3nk0@gmail.com>
2021-03-01 13:37:04 +0100

    send order button

--- app/src/main/java/com/example/musicshop/OrderActivity.kt
+++ app/src/main/java/com/example/musicshop/OrderActivity.kt
@@ -1,32 +1,37 @@
 package com.example.musicshop
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 private const val TAG = "OrderActivity"
 
 class OrderActivity : AppCompatActivity() {
     private var textViewUserName: TextView? = null
-    private var textViewGoodsName: TextView?= null
-    private var textViewGoodsQuantity: TextView?= null
-    private var textViewGoodsPrice: TextView?= null
+    private var textViewGoodsName: TextView? = null
+    private var textViewGoodsQuantity: TextView? = null
+    private var textViewGoodsPrice: TextView? = null
+    private var btnSendOrder: Button? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
+        title = "Your order"
+        val orderIntent = intent
 
-        var orderIntent = intent
-
-        textViewUserName= findViewById<EditText>(R.id.textView_OrderUserName)
-        textViewGoodsName=findViewById<TextView>(R.id.textView_OrderGoodsName)
-        textViewGoodsQuantity=findViewById<TextView>(R.id.textView_OrderQuantity)
-        textViewGoodsPrice=findViewById(R.id.textView_OrderPrice)
+        textViewUserName = findViewById<EditText>(R.id.textView_OrderUserName)
+        textViewGoodsName = findViewById<TextView>(R.id.textView_OrderGoodsName)
+        textViewGoodsQuantity = findViewById<TextView>(R.id.textView_OrderQuantity)
+        textViewGoodsPrice = findViewById(R.id.textView_OrderPrice)
 
         val userName = orderIntent.getStringExtra("userName")
-        val goodsName  = orderIntent.getStringExtra("goodsName")
+        val goodsName = orderIntent.getStringExtra("goodsName")
         val quantity = orderIntent.getStringExtra("quantity")
         val price = orderIntent.getStringExtra("orderPrice")
 
@@ -35,5 +40,23 @@ class OrderActivity : AppCompatActivity() {
         textViewGoodsQuantity?.text = quantity
         textViewGoodsPrice?.text = price
 
+        btnSendOrder = findViewById(R.id.btn_SendOrder)
+        btnSendOrder?.setOnClickListener {
+            val messageEt = "User name: $userName\nGoods name: $goodsName\nQuantity: $quantity\nPrice: $price"
+            val message = messageEt.trim()
+
+            sendOrder(message)
+        }
+    }
+
+    private fun sendOrder(message: String) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "message/rfc822"
+        intent.putExtra(Intent.EXTRA_TEXT, message)
+        try {
+            startActivity(Intent.createChooser(intent, "send mail..."))
+        } catch (e: Exception) {
+            Toast.makeText(this, "There are no clients installed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
